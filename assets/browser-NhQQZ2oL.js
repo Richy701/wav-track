86068e14c8d0224b9c7216d278456458d83a6da7
Richy701 <Richmondlamptey75@gmail.com>
2025-03-11 01:52:42 +0000

    Updates

---
+++ assets/browser-NhQQZ2oL.js
@@ -0,0 +1,2 @@
+import{n as e}from"./index-J8uE2cRJ.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
+//# sourceMappingURL=browser-NhQQZ2oL.js.map
